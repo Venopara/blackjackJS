eca8a4b0267f3b97400629002c4a36a573c6238c
SharpSeeEr <aaron@sharpseergroup.com>
2019-02-04 20:27:06 -0700

    Added card suits css and image

--- Blackjack.js
+++ Blackjack.js
@@ -1,6 +1,7 @@
 // JavaScript source code
 //Blackjack
-// By me
+// By Tristen Throckmorton
+// Updated by Aaron Throckmorton
 
 // The Card class represents a card, with suit, rank, and value.
 // Notice the class is declared as a const, which prevents us from
@@ -123,6 +124,23 @@ const GameOfBlackjack = function () {
       hit: document.getElementById('hit-button'),
       stay: document.getElementById('stay-button'),
       newHand: document.getElementById('new-hand-button')
+    },
+    endHand(winOrLose, message) {
+      if (message) message += ' '
+      ui.status.innerHTML = `${message}You ${winOrLose}!`
+      ui.buttons.newHand.classList.remove('hidden')
+      ui.buttons.hit.classList.add('hidden')
+      ui.buttons.stay.classList.add('hidden')
+    },
+    startHand() {
+      ui.status.innerHTML = ''
+      ui.dealer.classList.add('hide-first-card')
+      ui.buttons.newHand.classList.add('hidden')
+      ui.buttons.hit.classList.remove('hidden')
+      ui.buttons.stay.classList.remove('hidden')
+    },
+    setStatus(status) {
+      ui.status.innerHTML = status
     }
   }
 
@@ -146,17 +164,43 @@ const GameOfBlackjack = function () {
     ui.cards[playerIndex].appendChild(renderCard(card))
   }
 
+  function stay() {
+    ui.dealer.classList.remove('hide-first-card')
+
+    while (players[DEALER].handValue() < 17) {
+      hit(DEALER)
+    }
+
+    let dealerScore = players[DEALER].handValue()
+    let playerScore = players[PLAYER].handValue()
+
+    if (dealerScore > 21) {
+      ui.endHand('WIN', 'Dealer BUSTED!')
+    } else if (dealerScore >= playerScore) {
+      ui.endHand('LOSE')
+    } else {
+      ui.endHand('WIN')
+    }
+  }
+
   function dealNewHand() {
-    ui.dealer.classList.add('hide-first-card')
-    ui.buttons.newHand.classList.add('hidden')
+    ui.startHand()
 
     hit(DEALER)
     hit(PLAYER)
     hit(DEALER)
     hit(PLAYER)
 
-    ui.buttons.hit.classList.remove('hidden')
-    ui.buttons.stay.classList.remove('hidden')
+    let dealerScore = players[DEALER].handValue()
+    let playerScore = players[PLAYER].handValue()
+
+    if (dealerScore === 21 && playerScore === 21) {
+      ui.endHand('PUSH', 'Double Blackjack!')
+    } else if (dealerScore === 21) {
+      ui.endHand('LOSE', 'Dealer has Blackjack!')
+    } else if (playerScore  === 21) {
+      ui.endHand('WIN', 'Blackjack!')
+    }
   }
 
   ui.buttons.newGame.addEventListener('click', function (e) {
@@ -165,9 +209,12 @@ const GameOfBlackjack = function () {
 
   ui.buttons.hit.addEventListener('click', function (e) {
     hit(PLAYER)
-    if (players[PLAYER].handValue() > 21) {
+    let playerScore = players[PLAYER].handValue()
+    if (playerScore > 21) {
       // Busted!
-      ui.status.innerHTML = 'BUSTED'
+      ui.endHand('LOSE', 'BUSTED!')
+    } else if (playerScore == 21) {
+
     }
   })
 
@@ -183,12 +230,13 @@ const GameOfBlackjack = function () {
 
     let dealerScore = players[DEALER].handValue()
     let playerScore = players[PLAYER].handValue()
+
     if (dealerScore > 21) {
-      ui.status.innerHTML = 'Dealer BUSTED - You WIN!'
+      us.setStatus('Dealer BUSTED - You WIN!')
     } else if (dealerScore >= playerScore) {
-      ui.status.innerHTML = 'You LOSE!'
+      us.setStatus('You LOSE!')
     } else {
-      ui.status.innerHTML = 'You WIN!'
+      us.setStatus('You WIN!')
     }
     ui.buttons.newHand.classList.remove('hidden')
   })
@@ -197,115 +245,7 @@ const GameOfBlackjack = function () {
     dealNewHand()
   })
 
+  dealNewHand()
 }
 
-
-let textArea = document.getElementById('text-area');
-let newGameButton = document.getElementById("new-game");
-let hitButton = document.getElementById("hit");
-let stayButton = document.getElementById("stay");
-let player = document.getElementById("playercards");
-let dealer = document.getElementById("dealercards");
-
-
-let gameStarted = false,
-    gameOver = false,
-    playerWon = false,
-    dealerCards = [],
-    playerCards = [],
-    dealerScore = 0,
-    playerScore = 0,
-    deck = [];
-
-hitButton.style.display = 'none';
-stayButton.style.display = 'none';
-showStatus();
-
-
-newGameButton.addEventListener('click', function() {
-    gameStarted = true;
-    gameOver = false;
-    playerWon = false;
-
-    deck = createDeck();
-    shuffleDeck(deck);
-    dealerCards = [ getNextCard(), getNextCard() ];
-    dealerScore = dealerCards.reduce((a, b) => a + b.value, 0)
-    playerCards = [ getNextCard(), getNextCard() ];
-    playerScore = playerCards.reduce((a, b) => a + b.value, 0)
-
-    newGameButton.style.display = 'none';
-    hitButton.style.display = 'inline';
-    stayButton.style.display = 'inline';
-    showStatus();
-});
-
-hitButton.addEventListener('click', function() {
-    playerCards.push(getNextCard())
-    playerScore = playerCards.reduce((a, b) => a + b.value, 0)
-    showStatus();
-})
-
-
-function createDeck(){
-    let deck = []
-    for (let suitIdx = 0; suitIdx < suits.length; suitIdx++) {
-        for (let valueIdx = 2; valueIdx <= 10; valueIdx++) {
-            let card = {
-                suit: suits[suitIdx],
-                value: valueIdx,
-                name: valueIdx.toString()
-            };
-            deck.push( card );
-        }
-        deck.push({ suit: suits[suitIdx], value: 10, name: 'King' })
-        deck.push({ suit: suits[suitIdx], value: 10, name: 'Queen' })
-        deck.push({ suit: suits[suitIdx], value: 10, name: 'Jack' })
-        deck.push({ suit: suits[suitIdx], value: 11, name: 'Ace' })
-        // add Jack Queen King
-        // add Ace
-
-    }
-    return deck;
-}
-
-function shuffleDeck(deck) {
-    for (let i = 0; i < deck.length; i++) {
-        let swapIdx = Math.trunc(Math.random() * deck.length);
-        let tmp = deck[swapIdx];
-        deck[swapIdx] = deck[i]
-        deck[i] = tmp;
-    }
-}
-function getCardString(card) {
-    return card.name + ' of ' +card.suit + "<br>";
-}
-
-function showStatus() {
-    if (!gameStarted) {
-        textArea.innerText = 'Welcome to Blackjack!';
-        return;
-    }
-    let cardText = ''
-    for (let i = 0; i < playerCards.length; i++) {
-        cardText += getCardString(playerCards[i])
-    }
-    player.innerHTML = cardText + playerScore
-
-    cardText = ''
-    for (let i = 0; i < dealerCards.length; i++) {
-        cardText += getCardString(dealerCards[i])
-    }
-    dealer.innerHTML = cardText
-}
-
-function getNextCard() {
-    return deck.shift();
-}
-
-
-// console.log("Welcome to Blackjack!");
-
-// console.log("You are dealt:  ");
-// console.log(" " + getCardString (playerCards[0]) );
-// console.log(" " + getCardString (playerCards[1]) );
+let game = new GameOfBlackjack()
